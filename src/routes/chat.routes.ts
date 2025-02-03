9f47b0b1cffc3b9b51499f400d614e8ed3007f6c
ulianchenko <ulianchenko@gmail.com>
2025-02-03 22:14:34 +0200

    test chat messages were added

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -4,10 +4,19 @@ const router = express.Router({ mergeParams: true });
 
 router.post('/', async (req: Request, res: Response): Promise<void> => {
   console.log('req from bx24:', req);
+  const messages = [
+    'Hmmm... nice question, it seems I do not know the answer',
+    'Let me think about it a little bit...',
+    'Your questions show that you are a very smart person',
+    'Live me alone please, I need some rest',
+    'Why do you have so many questions? Talk to someone else',
+    'I have a lazy day today. Do not disturb me',
+    'What a stupid question. Do you have nothing to do?'
+  ];
 
 res.status(200).json({
     response: {
-      message: 'Hi, the weather is greate',
+      text: `${messages[Math.floor(Math.random()*7)]}`
     },
   });
 });
